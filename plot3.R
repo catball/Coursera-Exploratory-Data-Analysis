5398fdd9501019cbe438eacb231ad7b1a16ef02b
catball <sarenazhao@gmail.com>
2015-03-22 14:57:28 -0700

    project 2

---
+++ plot3.R
@@ -0,0 +1,33 @@
+# Load the NEI & SCC data frames.
+NEI <- readRDS("~/Documents/R/Exploratory_Data_Analysis/summarySCC_PM25.rds")
+SCC <- readRDS("~/Documents/R/Exploratory_Data_Analysis/Source_Classification_Code.rds")
+
+# Subset NEI data by Baltimore's fip.
+baltimoreNEI <- NEI[NEI$fips=="24510",]
+
+# Aggregate using sum the Baltimore emissions data by year
+aggTotalsBaltimore <- aggregate(Emissions ~ year, baltimoreNEI,sum)
+
+# Of the four types of sources indicated by the type (point, nonpoint, onroad, nonroad) variable, 
+# which of these four sources have seen decreases in emissions from 1999–2008 for Baltimore City? 
+# Which have seen increases in emissions from 1999–2008? 
+# Use the ggplot2 plotting system to make a plot answer this question.
+
+# Generate the graph in the same directory as the source code
+png("~/Documents/R/Exploratory_Data_Analysis/plot3.png",width=480,height=480,units="px",bg="transparent")
+
+library(ggplot2)
+
+ggp <- ggplot(baltimoreNEI,aes(factor(year),Emissions,fill=type)) +
+        geom_bar(stat="identity") +
+        theme_bw() + guides(fill=FALSE)+
+        facet_grid(.~type,scales = "free",space="free") + 
+        labs(x="year", y=expression("Total PM"[2.5]*" Emission (Tons)")) + 
+        labs(title=expression("PM"[2.5]*" Emissions, Baltimore City 1999-2008 by Source Type"))
+
+print(ggp)
+
+dev.off()
+
+
+
---
+++ plot4.R
@@ -0,0 +1,28 @@
+# Load the NEI & SCC data frames.
+NEI <- readRDS("~/Documents/R/Exploratory_Data_Analysis/summarySCC_PM25.rds")
+SCC <- readRDS("~/Documents/R/Exploratory_Data_Analysis/Source_Classification_Code.rds")
+
+# Subset coal combustion related NEI data
+combustionRelated <- grepl("comb", SCC$SCC.Level.One, ignore.case=TRUE)
+coalRelated <- grepl("coal", SCC$SCC.Level.Four, ignore.case=TRUE) 
+coalCombustion <- (combustionRelated & coalRelated)
+combustionSCC <- SCC[coalCombustion,]$SCC
+combustionNEI <- NEI[NEI$SCC %in% combustionSCC,]
+
+# Generate the graph in the same directory as the source code
+png("~/Documents/R/Exploratory_Data_Analysis/plot4.png",width=480,height=480,units="px",bg="transparent")
+
+library(ggplot2)
+
+ggp <- ggplot(combustionNEI,aes(factor(year),Emissions/10^5)) +
+        geom_bar(stat="identity",fill="grey",width=0.75) +
+        theme_bw() +  guides(fill=FALSE) +
+        labs(x="year", y=expression("Total PM"[2.5]*" Emission (10^5 Tons)")) + 
+        labs(title=expression("PM"[2.5]*" Coal Combustion Source Emissions Across US from 1999-2008"))
+
+print(ggp)
+
+dev.off()
+
+
+
